33fb99085a6b88fa69946f74b3b654aa13210e32
Bokyoung Ku <bokyounk@andrew.cmu.edu>
2025-06-16 15:35:59 -0400

    Measure consumed drawing object time

--- src/DisplayGUI.h
+++ src/DisplayGUI.h
@@ -258,6 +258,8 @@ private: // User declarations
     MapProvider* currentMapProvider;
     bool LoadMapFromInternet;
     void ReloadMapProvider();
+    bool isSBSConnected;              // Manage SBS connect status
+    bool isSBSPlayBacked;             // Manage SBS PlayBack status
 
 public:  // User declarations
     __fastcall TForm1(TComponent *Owner);
